2554b4d9251d402062268132a4d958f6658b2096
lusy <vaseva@mi.fu-berlin.de>
2015-03-10 16:12:51 +0100

    convert all articles to plain text

--- src/cleaner.py
+++ src/cleaner.py
@@ -1,5 +1,8 @@
 #-*- coding: utf-8 -*-
 
+import sys
+import os
+from os import path
 from pattern import web
 
 def to_plaintext(article_id):
@@ -8,12 +11,26 @@ def to_plaintext(article_id):
     '''
     file_article = open(article_id, 'r')
     article = file_article.read()
-    print web.plaintext(article)
-    return web.plaintext(article)
+    article_plain = web.plaintext(article)
+    file_article.close()
+    return article_plain
 
-def verticalize(plaintext):
-    '''
-    verticalizes the article
-    '''
 
-to_plaintext('test-article')
+def main():
+    '''
+    applies to_plaintext() to all articles in articles-raw/
+    and saves the plaintext artivles to articles-plain/
+    '''
+    articles_raw = [f for f in os.listdir('articles-raw/') if path.isfile(path.join('articles-raw/', f))]
+    for article_id in articles_raw:
+        print "Cleaning article %s" % article_id
+        file_path_raw = 'articles-raw/%s' % article_id
+        article_plain = to_plaintext(file_path_raw)
+        file_path_plain = 'articles-plain/%s' % article_id
+        f = open(file_path_plain, 'w')
+        f.write(article_plain.encode('utf-8'))
+        f.close()
+
+if __name__ == '__main__':
+    status = main()
+    sys.exit(status)
