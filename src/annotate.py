03c21af9bd9cf6daba4ba3057caa0bea63449f59
lusy <vaseva@mi.fu-berlin.de>
2015-03-14 10:54:16 +0100

    refactor annotate(): add comments + write only EN and UNK to files

--- src/annotate.py
+++ src/annotate.py
@@ -1,13 +1,14 @@
 #-*- coding: utf-8 -*-
 
 import sys
-import nltk
 import codecs
 import os
+import nltk
 from os import path
 from nltk.corpus import PlaintextCorpusReader
+import text_local
 
-def annotate(punctuation, ignore_en, tokens_en, tokens_common, reg_dicts_tokens):
+def annotate(a_path, punctuation, ignore_en, tokens_en, tokens_common, reg_dicts_tokens):
     '''
     annotates each word for an article according
     to the dictionary in which it was found
@@ -23,11 +24,11 @@ def annotate(punctuation, ignore_en, tokens_en, tokens_common, reg_dicts_tokens)
     annotated_article = []
 
     # read articles as utf-8!
-    with codecs.open('data/articles-plain/45', encoding='utf-8') as article_plain_file:
+    with codecs.open(a_path, encoding='utf-8') as article_plain_file:
         article_plain= article_plain_file.read()
 
         # remove special spanish punctuation characters which are treated
-        # as part of the upcoming word by the nltk word tokenizer (default lang: en)
+        # as a part of the upcoming word by the nltk word tokenizer (default lang: en)
         # add blank in front of '.' so that it's recognized as a separate token
         translations = (
                 (u'¡', ''),
@@ -37,12 +38,9 @@ def annotate(punctuation, ignore_en, tokens_en, tokens_common, reg_dicts_tokens)
                 )
         for from_str, to_str in translations:
             article_plain = article_plain.replace(from_str, to_str)
-        #print article_plain
 
         # tokenize
         tokens_article = nltk.word_tokenize(article_plain)
-        #tokens = esp_tokenizer.word_tokenize(article_plain)
-        #print tokens
 
     for token in tokens_article:
         annotation = []
@@ -76,18 +74,87 @@ def annotate(punctuation, ignore_en, tokens_en, tokens_common, reg_dicts_tokens)
 
         annotated_article.append((token,annotation))
 
+    #TODO: write annotated article to file in an appropriate format
+    # what is an appropriate format? (csv, text: token/ANN1_ANN2_ANN3..., xml)?
 
-    #print tokens_article[0].lower() in tokens_common
-    #print tokens_article[0].lower() in tokens_en
+    # build concordances for the whole article
+    concordanceIndex = text_local.ConcordanceIndex(tokens_article, key=lambda s:s.lower())
 
-    #print tokens_article
-    #print annotated_article
+    a_id = a_path.split('/')[-1]
+
+    ''' Comment out for concordances of all tokens with EN in annotations
+    # write selected concordances to a file
+    # (which contains concordances for the whole corpus)
+    with codecs.open('data/concordances', 'a', encoding='utf-8') as out_file:
+        out_file.write("Article # %s\n" % a_id)
+
+        # we want to print the concordances for a give token just once
+        # if the token appears multiple times in an article
+        # en_tokens is a cache for the concordances we already printed
+        en_tokens = []
+
+        # bypass the Text() class for writing concordances to file
+        #test_text = nltk.Text(tokens_article)
+        for (token,annotation) in annotated_article:
+            # filter all tokens where EN among annotations
+            if 'EN' in annotation and token not in en_tokens:
+                en_tokens.append(token)
+                ta = '[%s, %s]\n' % (token, annotation)
+                out_file.write(ta)
+                out_file.write(concordanceIndex.return_concordance(token, width=79, lines=25))
+                out_file.write("\n")
+
+        out_file.write("-" * 60)
+        out_file.write("\n\n")
+    '''
+
+    ''' Block for computing concordances for EN annotations only and UNK annotations'''
+    # write selected concordances to files
+    # (which contain concordances for the whole corpus)
+    out_file_EN = codecs.open('data/concordances_EN', 'a', encoding='utf-8')
+    out_file_UNK = codecs.open('data/concordances_UNK', 'a', encoding='utf-8')
+
+    out_file_EN.write("Article # %s\n" % a_id)
+    out_file_UNK.write("Article # %s\n" % a_id)
+
+    # we want to print the concordances for a give token just once
+    # if the token appears multiple times in an article
+    # en_tokens is a cache for the EN concordances we already printed
+    en_tokens = []
+    # unk_tokens is a cache for the UNK concordances we already printed
+    unk_tokens = []
 
     for (token,annotation) in annotated_article:
-        if 'EN' in annotation:
-            print (token,annotation)
+        # filter all tokens with annotation EN only
+        if 'EN' in annotation and token not in en_tokens and len(annotation) == 1:
+            en_tokens.append(token)
+            ta = '[%s, %s]\n' % (token, annotation)
+            out_file_EN.write(ta)
+            out_file_EN.write(concordanceIndex.return_concordance(token, width=79, lines=25))
+            out_file_EN.write("\n")
+        # filter all tokens with annotation UNK
+        if 'UNK' in annotation and token not in unk_tokens:
+            unk_tokens.append(token)
+            ta_unk = '[%s, %s]\n' % (token, annotation)
+            out_file_UNK.write(ta_unk)
+            out_file_UNK.write(concordanceIndex.return_concordance(token, width=79, lines=25))
+            out_file_UNK.write("\n")
 
-    #TODO: write to file in an appropriate format
+    out_file_EN.write("-" * 60)
+    out_file_EN.write("\n\n")
+
+    out_file_UNK.write("-" * 60)
+    out_file_UNK.write("\n\n")
+
+    out_file_EN.close()
+    out_file_UNK.close()
+
+
+    #test_text = nltk.Text(tokens_article)
+    #test_text.concordance(u'balance')
+    #test_text.concordance(u'pero')
+    #test_text.similar(u'balance')
+
 
 
 def main():
@@ -100,7 +167,8 @@ def main():
     punctuation = [u'.', u'!', u'?', u':', u',', u';', u'-', u'"', u"'", u'(', u')']
 
     # ignore from English dictionary (words that are far more probable to be in esp)
-    ignore_en = [u'para', u'las', u'nos', u'con', u'no', u'o']
+    ignore_en = [u'para', u'las', u'nos', u'con', u'vive', u'persona', u'peso', u'lee', u'mis', u'dos', u'etc', u'hombre',
+                 u'placer', u'hoy', u'perfecta', u'mil', u'dolor']
 
     # import dictionaries (as utf-8!) --> should be done here, not importing dictionaries 2000 times
     with codecs.open('data/dictionaries-common/es_ALL', encoding='utf-8') as common_dict_file:
@@ -113,7 +181,7 @@ def main():
 
     # import all the regional dicts
     reg_dicts_ids = [f for f in os.listdir('data/dictionaries-common/') if path.isfile(path.join('data/dictionaries-common/', f)) and f.endswith('reg')]
-    print reg_dicts_ids
+    #print reg_dicts_ids
 
     reg_dicts_tokens = {}
     for dict_id in reg_dicts_ids:
@@ -133,22 +201,13 @@ def main():
 
 
     # should it get dictionaries as param?
-    annotate(punctuation, ignore_en, tokens_en, tokens_common, reg_dicts_tokens)
 
     # TODO: annotate all articles
-
-    #print tokens[0].lower() in tokens_ar
-    #print tokens[0].lower() in tokens_common
-    #print tokens
-
-    #test_text = nltk.Text(tokens)
-    #test_text.concordance('sueños')
-    #test_text.concordance('pero')
-    #test_text.similar('sueños')
-
-
-
-
+    articles_ids = [f for f in os.listdir('data/articles-plain/') if path.isfile(path.join('data/articles-plain/', f))]
+    for a_id in articles_ids:
+        print "Annotating article #%s" % a_id
+        a_path = 'data/articles-plain/%s' % a_id
+        annotate(a_path, punctuation, ignore_en, tokens_en, tokens_common, reg_dicts_tokens)
 
 if __name__ == '__main__':
     status = main()
